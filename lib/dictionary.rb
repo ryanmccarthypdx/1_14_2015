06f113b662c6d1632d5f454a60198400416f2187
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-14 16:02:33 -0800

    Add functional files before trying to make it multi-class

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,17 +2,17 @@ class Term
   @@all_terms = []
 
   define_method(:initialize) do |word, definition|
-    @word = word
-    @definition = definition
-    @entry = [@word, @definition]
+    @word = [word]
+    @definition = [definition]
+    # @entry = [@word, @definition]
   end
 
   define_method(:word) do
-    @word
+    @word.join("; ").to_s()
   end
 
   define_method(:definition) do
-    @definition
+    @definition.join("; ").to_s()
   end
 
   define_method(:save) do
@@ -27,14 +27,33 @@ class Term
     @@all_terms
   end
 
-  define_singleton_method(:redefine) do |search_word, new_definition|
+  # define_singleton_method(:redefine) do |search_word, new_definition|
+  #   @@all_terms.each() do |entry|
+  #     if entry.word().==(search_word)
+  #       entry.definition().replace(new_definition)
+  #     end
+  #   end
+  # end
+
+  define_singleton_method(:search) do |search_word|
     @@all_terms.each() do |entry|
-      if entry.word().==(search_word)
-        entry.definition().replace(new_definition)
+      if entry.word().include?(search_word)
+        return entry
       end
     end
   end
 
+  define_method(:redefine) do |new_definition|
+    arrayizer = [new_definition]
+    @definition = arrayizer
+  end
 
+  define_method(:append) do |append_definition|
+    @definition.push(append_definition)
+  end
+
+  define_method(:woppend) do |woppend_word|
+    @word.push(woppend_word)
+  end
 
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -2,6 +2,10 @@ require('dictionary')
 require('rspec')
 
 describe(Term) do
+  before() do
+    Term.clear()
+  end
+
   describe('#word') do
     it("returns the word that we have defined") do
       test_entry = Term.new("van", "large car for camping")
@@ -29,13 +33,34 @@ describe(Term) do
     it("will edit the definition") do
     test_entry1 = Term.new("van", "large car for camping")
     test_entry1.save()
-    Term.redefine("van", "vehicle drive with big")
+    Term.search("van").redefine("vehicle drive with big")
     expect(test_entry1.definition()).to(eq("vehicle drive with big"))
     end
   end
 
+  describe('.search') do
+    it("returns the entire entry for a searched term") do
+    test_entry1 = Term.new("van", "large car for camping")
+    test_entry2 = Term.new("car", "small vehicle for driving")
+    test_entry1.save()
+    test_entry2.save()
+    expect(Term.search("van")).to(eq(test_entry1))
+    end
+  end
 
+  describe('#append') do
+    it("adds a second or third, etc. definition to single headword") do
+      test_entry1 = Term.new("van", "large car for camping")
+      test_entry1.append("small vehicle for driving")
+      expect(test_entry1.definition()).to(eq("large car for camping; small vehicle for driving"))
+    end
+  end
 
-
-
+  describe('#woppend') do
+    it("adds a second or third, etc headword to an entry") do
+      test_entry1 = Term.new("van", "large car for camping")
+      test_entry1.woppend("Auto")
+      expect(test_entry1.word()).to(eq("van; Auto"))
+    end
+  end
 end
